5934a75c3b43c35cf13520141df313d7cb449176
Qaiser K. Durrani <qkdurrani@gmail.com>
2022-03-31 10:45:18 +0500

    Subject line: Added some javascript
    Description:
    Added some javascript to have some dynamic behavior

--- scripts/main.js
+++ scripts/main.js
@@ -32,3 +32,51 @@ myImage.onclick = function() {
 		myImage.setAttribute('src', 'images/funTime.jpg');
 	}
 }
+
+//===
+// button; to modify the user
+//===
+let myButton = document.querySelector('button');
+let myUserName = document.querySelector('h2');
+
+/*
+	API localStorage, which allows us to store data in the
+	browser and retrieve it later. We use localStorage's setItem()
+	function to create and store a data item called 'name',
+	setting its value to the myName variable which contains
+	the user's entry for the name.
+*/
+
+/*
+	ask the user to input a name, that will be used in
+	the welcome message
+*/
+function setUserName() {
+	let myName = prompt('Please enter your name.');
+
+	//make sure that user has entered something
+	if(!myName) {
+		setUserName();
+	} else {
+		localStorage.setItem('name', myName);
+		myUserName.textContent = 'Welcome! ' + myName;
+	}
+}
+
+/*
+	if the name has not been entered then ask the user
+	to enter it else, change the welcome message
+*/
+if(!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	let storedName = localStorage.getItem('name');
+	myUserName.textContent = 'Welcome! ' + storedName;
+}
+
+/*
+	when the button is clicked do this operation
+*/
+myButton.onclick = function() {
+	setUserName();
+}
